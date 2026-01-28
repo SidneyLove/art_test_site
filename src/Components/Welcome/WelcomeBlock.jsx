6ba439bb6f8d80a440058b77ca11bc5e571aad9c
Влад Кирсанов <vlad_kirsanov@MacBook-Air-Vlad.local>
2026-01-28 18:05:36 +0300

    Updated the mobile version of the website

--- src/Components/Welcome/WelcomeBlock.jsx
+++ src/Components/Welcome/WelcomeBlock.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import welcome_img from '../../img/welcome_img.png';
 import { SmoothLink } from '../SmoothLink';
 import { WelcomeCard } from './WelcomeCard';
@@ -6,28 +5,32 @@ import { WelcomeCard } from './WelcomeCard';
 export const WelcomeBlock = () => {
 
     return (
-        <div className="container-fluid overflow-hidden welcom-bg mt-3 p-0 px-3 pt-5">
-            <div className='container row mx-auto py-4 px-0'>
-                <div className="col-6 d-flex flex-column align-items-start p-0">
-                    <h1 className='text-uppercase header-color responsive-header'>
-                        <span className='responsive-header-welcome header-font'>Петербург</span> <br/>
-                        <span className='header-font'>сквозь года</span>
-                    </h1>
-                    <p className='responsive-text blue-dark text-uppercase'><b>с Владимиром Заруцким</b></p>
+        <div className="container-fluid welcom-bg mt-3 px-3 pt-5 pb-0">
+            <div className='container d-flex flex-column mx-auto pt-4 px-0'>
+                <div className='d-flex flex-column-reverse flex-md-row'>
+                    <div className="col-12 col-md-6 d-flex flex-column align-items-center align-items-md-start p-0">
+                        <h1 className='text-uppercase header-color responsive-header'>
+                            <span className='responsive-header-welcome header-font'>Петербург</span> <br/>
+                            <span className='header-font'>сквозь года</span>
+                        </h1>
+                        <p className='responsive-text blue-dark text-uppercase'><b>с Владимиром Заруцким</b></p>
                     
-                    <ul className='responsive-text px-3 py-4'>
-                        <li>Нарисуйте <span className='blue'>12 пейзажей:</span> от ретро Петербурга до сегодняшних дней</li>
-                        <li>Изучите <span className='blue'>способы передачи особенностей архитектуры</span> с помощью акварели</li>
-                        <li>Научитесь передавать <span className='blue'>художественные образы и замыслы</span></li>
-                    </ul>
-                    
+                        <ul className='responsive-text px-3 py-4'>
+                            <li>Нарисуйте <span className='blue'>12 пейзажей:</span> от ретро Петербурга до сегодняшних дней</li>
+                            <li>Изучите <span className='blue'>способы передачи особенностей архитектуры</span> с помощью акварели</li>
+                            <li>Научитесь передавать <span className='blue'>художественные образы и замыслы</span></li>
+                        </ul>
+                        
                         <SmoothLink href='#about_of_course'><p className='btn-bg w-auto text-uppercase text-white responsive-text py-2 px-5 rounded'>узнать подробности</p></SmoothLink>
-                    
+                        
+                    </div>
+                    <div className="d-flex col-12 col-md-6 p-0 align-items-center align-items-md-start overflow-hidden">
+                        <img className='img-fluid' src={welcome_img}/>
+                    </div>
                 </div>
-                <div className="col-6 p-0">
-                    <img className='img-fluid' src={welcome_img}/>
-                </div>
-                <div className='col-12 d-flex flex-column flex-md-row justify-content-center align-items-center' style={{marginBottom: '-7vh'}}>
+                
+                
+                <div className='col-12 d-flex flex-column flex-md-row justify-content-center align-items-center' style={{ marginBottom: '-5vh' }}>
                     <WelcomeCard name='Материал:' text='акварель'/>
                     <div style={{width: '0.5rem'}}/>
                     <WelcomeCard name='Количество картин:' text='12'/>
@@ -36,53 +39,8 @@ export const WelcomeBlock = () => {
                     <div style={{width: '0.5rem'}}/>
                     <WelcomeCard name='Доступ:' text='бессрочный'/>
                 </div>
-                
             </div>
             
         </div>
     )
-=======
-import welcome_img from '../../img/welcome_img.png';
-import { SmoothLink } from '../SmoothLink';
-import { WelcomeCard } from './WelcomeCard';
-
-export const WelcomeBlock = () => {
-
-    return (
-        <div className="container-fluid overflow-hidden welcom-bg mt-3 p-0 px-3 pt-5">
-            <div className='container row mx-auto py-4 px-0'>
-                <div className="col-6 d-flex flex-column align-items-start p-0">
-                    <h1 className='text-uppercase header-color responsive-header'>
-                        <span className='responsive-header-welcome header-font'>Петербург</span> <br/>
-                        <span className='header-font'>сквозь года</span>
-                    </h1>
-                    <p className='responsive-text blue-dark text-uppercase'><b>с Владимиром Заруцким</b></p>
-                    
-                    <ul className='responsive-text px-3 py-4'>
-                        <li>Нарисуйте <span className='blue'>12 пейзажей:</span> от ретро Петербурга до сегодняшних дней</li>
-                        <li>Изучите <span className='blue'>способы передачи особенностей архитектуры</span> с помощью акварели</li>
-                        <li>Научитесь передавать <span className='blue'>художественные образы и замыслы</span></li>
-                    </ul>
-                    
-                        <SmoothLink href='#about_of_course'><p className='btn-bg w-auto text-uppercase text-white responsive-text py-2 px-5 rounded'>узнать подробности</p></SmoothLink>
-                    
-                </div>
-                <div className="col-6 p-0">
-                    <img className='img-fluid' src={welcome_img}/>
-                </div>
-                <div className='col-12 d-flex flex-column flex-md-row justify-content-center align-items-center' style={{marginBottom: '-7vh'}}>
-                    <WelcomeCard name='Материал:' text='акварель'/>
-                    <div style={{width: '0.5rem'}}/>
-                    <WelcomeCard name='Количество картин:' text='12'/>
-                    <div style={{width: '0.5rem'}}/>
-                    <WelcomeCard name='Продолжительность курса:' text='20 часов'/>
-                    <div style={{width: '0.5rem'}}/>
-                    <WelcomeCard name='Доступ:' text='бессрочный'/>
-                </div>
-                
-            </div>
-            
-        </div>
-    )
->>>>>>> 7603237923d99bbac572894bdcbb5260772bb522
 }
